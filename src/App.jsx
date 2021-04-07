7df2fc1af708274f2fa3033a9b50c58dbe8c28d5
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-04-07 16:53:18 +0200

    Switcher Provider to index.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,35 @@ import Layout from "./containers/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
-import { Provider, connect } from "react-redux";
-import thunk from "redux-thunk";
-import burgerReducer from "./store/reducers/burgerReducer";
-import orderReducer from "./store/reducers/orderReducer";
+
+import { connect } from "react-redux";
+
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import AuthComponent from "./containers/Auth/Auth";
-import authReducer from "./store/reducers/authReducer";
+
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 
-const rootReducer = combineReducers({
-  burgerBuilder: burgerReducer,
-  order: orderReducer,
-  auth: authReducer,
-});
+function App() {
+  // const { onTryAutoSignUp } = props;
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-  rootReducer,
-  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
-);
-
-function App(props) {
-  const { onTryAutoSignUp } = props;
-
-  useEffect(() => {
-    onTryAutoSignUp();
-  }, []);
+  // useEffect(() => {
+  //   onTryAutoSignUp();
+  // }, []);
 
   return (
     <div>
-      <Provider store={store}>
-        <Router>
-          <Layout>
-            <Switch>
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/auth" exact component={AuthComponent} />
-              <Route path="/orders" exact component={Orders} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/" exact component={BurgerBuilder} />
-            </Switch>
-          </Layout>
-        </Router>
-      </Provider>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/auth" exact component={AuthComponent} />
+            <Route path="/orders" exact component={Orders} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
@@ -59,4 +43,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import burgerReducer from "./store/reducers/burgerReducer";
+import orderReducer from "./store/reducers/orderReducer";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import authReducer from "./store/reducers/authReducer";
+
+const rootReducer = combineReducers({
+  burgerBuilder: burgerReducer,
+  order: orderReducer,
+  auth: authReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
